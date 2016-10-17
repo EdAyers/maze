883bbad6c97ec1fb24d3313b80ac7fd0e4a0f50d
fsherry <fsherry@users.noreply.github.com>
2016-10-17 15:23:27 +0100

    Update simple_goody.py

--- simple_goody.py
+++ simple_goody.py
@@ -5,19 +5,22 @@ from maze import Goody, UP, LEFT, DOWN, RIGHT, STAY, PING
 class SimpleGoody(Goody):
 
     def take_turn(self, _obstruction, _ping_response):
-        players = _ping_response.keys()
-        baddy = next(player for player in players if isinstance(player, Baddy))
-        other_goody = next(player for player in players if isinstance(player, Goody) and player != self)
-   
-        weights = [(_ping_response[other_goody] - Position(x)).l1_norm() / (_ping_response[baddy] - Position(x)).l1_norm() 
-                                                                for x in [(0, 1), (1, 0), (0, -1), (-1, 0)]]
-        
-        positions = [UP, RIGHT, DOWN, LEFT]
+        if _ping_response:
+            players = _ping_response.keys()
+            baddy = next(player for player in players if isinstance(player, Baddy))
+            other_goody = next(player for player in players if isinstance(player, Goody) and player != self)
 
-        for (k, v) in zip(weights, positions).sort(key = lambda tup: tup[0]):
-            if not _obstruction[v]:
-                return v
-            
+            weights = [(_ping_response[other_goody] - Position(x)).l1_norm() / (_ping_response[baddy] - Position(x)).l1_norm() 
+                                                                    for x in [(0, 1), (1, 0), (0, -1), (-1, 0)]]
+
+            positions = [UP, RIGHT, DOWN, LEFT]
+
+            for (k, v) in zip(weights, positions).sort(key = lambda tup: tup[0]):
+                if not _obstruction[v]:
+                    return v
+        else: 
+            return STAY
+
         
 
             
